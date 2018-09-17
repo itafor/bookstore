5665d89d17a74414dc158e1c9a8e52b1b6c0b0fc
Your Name <you@example.com>
2018-09-17 10:45:38 -0700

    Initial commit

--- app/User.php
+++ app/User.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-
+use App\Book;
 class User extends Authenticatable
 {
     use Notifiable;
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password'
     ];
 
     /**
@@ -26,4 +26,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //Creating relationship between a user and books
+    //a user can add as many books as he wish 
+    public function books()
+    {
+      return $this->hasMany(Book::class);
+    }
 }
